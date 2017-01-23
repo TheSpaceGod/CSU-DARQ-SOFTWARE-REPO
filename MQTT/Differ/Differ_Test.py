92705a16540f7efa8e02daada8f37d6d63f961e3
TheSpaceGod <john.rya@hotmail.com>
2017-01-22 21:15:30 -0700

    Latest Changes

---
+++ MQTT/Differ/Differ_Test.py
@@ -0,0 +1,9 @@
+import Differ
+#import DifferWriter
+
+pathA = '/media/sf_SpaceGod/Documents/Workspace/CSU-DARQ-SOFTWARE-REPO/MQTT/Folder Observer/TestDir/Test.txt'
+pathB = '/media/sf_SpaceGod/Documents/Workspace/CSU-DARQ-SOFTWARE-REPO/MQTT/Folder Observer/TestDir/Test2.txt'
+
+foo = Differ(pathA, pathB)
+result = foo.run()
+print result
