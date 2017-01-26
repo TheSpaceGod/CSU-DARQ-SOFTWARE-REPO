1ac47fff6ad3d295f3b79dc341600749dbe9e0f9
JordenHam <jordenh18@gmail.com>
2017-01-26 12:35:00 -0700

    Barebones file system observer class. Just watching for modifications right now, but watchdog is powerful and this can have well more functionality in it.

--- MQTT/Folder Observer/fswatch.py
+++ MQTT/Folder Observer/fswatch.py
@@ -16,30 +16,14 @@ pip install configparser
 
 import os, os.path, datetime, time
 import configparser
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import watchdog.events as events
 
 # config parameters
-local_path = ""
-remote_host = ""
-remote_path = ""
-
-# list of files to ignore, simple substring match
-ignore_list = ['.svn', '.DS_Store', '.git']
-
-
-def load_config():
-    global local_path, remote_host, remote_path
-    configParser = configparser.RawConfigParser()
-    configFilePath = "./fswatch.conf"
-    if not os.path.isfile(configFilePath):
-        print("Config file fswatch.conf not found")
-        os.exit(1)
-
-    configParser.read(configFilePath)
-    local_path = configParser.get('local', 'path')
-    remote_host = configParser.get('remote', 'server')
-    remote_path = configParser.get('remote', 'path')
+local_path = "/Users/jordenh18/Desktop/SENIOR DESIGN/GitRepo/CSU-DARQ-SOFTWARE-REPO/MQTT/Folder Observer/fsTestDir/test.txt"
+dir_path = "/Users/jordenh18/Desktop/SENIOR DESIGN/GitRepo/CSU-DARQ-SOFTWARE-REPO/MQTT/Folder Observer/fsTestDir"
 
 
 def display(str):
@@ -50,26 +34,18 @@ def display(str):
 # handles sync event actions, only modified
 class MySyncHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        global local_path, remote_host, remote_path
-        filename = event.src_path
-        remote_file = filename.replace(local_path, '')  # switch local path to remote
-        for ig in ignore_list:  # check ignore list
-            if ig in filename:
-                return
-
-        # basic rsync to remote server
-        cmd = " rsync -cazq --del %s %s:%s%s " % (filename, remote_host, remote_path, remote_file)
-        display("Syncing %s " % filename)
-        os.system(cmd)
+        #call differ
+        print("modified")
 
 
 ## main loop
 def main():
-    global local_path, remote_host, remote_path
-    load_config();
-
+    global local_path#, remote_host, remote_path
+    #load_config();
+    fsEvent = events
+    #fsHandler = events.FileSystemEventHandler.on_modified(fsEvent)
     observer = Observer()
-    observer.schedule(MySyncHandler(), ".", recursive=True)
+    observer.schedule(MySyncHandler(),dir_path , recursive=True)
     observer.start()
     try:
         while True:
