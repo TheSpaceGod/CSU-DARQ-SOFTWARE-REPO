ee7dc1127a27b866ca562bb58047ffff128d0849
TheSpaceGod <john.rya@hotmail.com>
2017-02-01 20:30:08 -0700

    Newest Changes

--- MQTT/Client.py
+++ MQTT/Client.py
@@ -3,9 +3,42 @@
 import paho.mqtt.client as paho
 
 class MQTTclient:
-    def on_connect(client, userdata, flags, rc):
-        print(“CONNACK received with code % d.” % (rc))
+    def __init__(self):
+        # paho.Client(client_id=””, clean_session=True, userdata=None, protocol=paho.MQTTv31)
+        self.client = paho.Client()
+        # At most once (0), At least once (1), Exactly once (2)
+        # http://www.hivemq.com/blog/mqtt-essentials-part-6-mqtt-quality-of-service-levels
+        self.QOS = 2
 
-    client = paho.Client()
-    client.on_connect = on_connect
-    client.connect(“broker.mqttdashboard.com”, 1883)
+    # Connect to MQTT Broker
+    def connect(self, host, port):
+        # client.connect(host=”localhost”, port=1883, keepalive=60, bind_address=””)
+        self.client.connect(host, port)
+
+    # Reports connections
+    def on_connect(client, userdata, flags, rc):
+        print("CONNACK received with code % d." % (rc))
+
+    # Posts a message on a topic
+    def publish(self, topic, msg):
+        (rc, mid) = self.client.publish(topic, msg, qos = self.QOS) # Returns (Return Code, Message ID)
+
+    # Reports publications on topics
+    def on_publish(client, userdata, mid):
+        print("mid: " + str(mid))
+
+    # Subscribe to topic
+    def subscribe(self, topic):
+        self.client.subscribe(topic)
+
+    # Reports subscriptions
+    def on_subscribe(client, userdata, mid, granted_qos):
+        print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+    # Unsubscribe from topic
+    def unsubscribe(self, topic):
+        self.client.unsubscribe(topic)
+
+    # IDK yet
+    def on_message(client, userdata, msg):
+        print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
