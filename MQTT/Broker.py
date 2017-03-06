169e9a5818e7f394db0e5214a1dc58278ac64be8
TheSpaceGod <john.rya@hotmail.com>
2017-03-06 15:06:00 -0700

    Newest Changes

--- MQTT/Broker.py
+++ MQTT/Broker.py
@@ -2,23 +2,40 @@
 
 import os
 import subprocess
+import time
 
 class Broker:
     def __init__(self):
         euid = os.geteuid()
         if euid != 0:
-            raise(EnvironmentError, "Need to be root to run Mosquitto Broker.")
+            print("Need to be root to run Mosquitto Broker.")
+            exit()
+        self.PnullOUT = open(os.devnull, 'w')   # Disregard process output
+        try:
+            self.Bproc = subprocess.Popen('ls', stdout=self.PnullOUT, stderr=subprocess.PIPE)   # Dummy Process to initialize Popen
+        except subprocess.CalledProcessError:
+            print("Dummy Process 'ls' failed, are you running linux? Exiting Broker.")
             exit()
 
     def start(self):
-        subprocess.call("sudo /etc/init.d/mosquitto start")
+        if not self.Bproc.poll() == None:
+            try:
+                self.Bproc = subprocess.Popen(['sudo', '/etc/init.d/mosquitto', 'start'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            except subprocess.CalledProcessError as e:
+                print("Failed to start Mosquitto Broker. Error: ", e)
+        else:
+            print("Broker already running, run the stop function to stop Broker.")
 
     def stop(self):
-        subprocess.call("sudo /etc/init.d/mosquitto stop")
+        try:
+            p = subprocess.Popen(['sudo', '/etc/init.d/mosquitto', 'stop'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            while self.Bproc.poll() == None:
+                time.sleep(1)
+        except subprocess.CalledProcessError as e:
+            print("Error encountered while stopping Mosquitto Broker. Error: ", e)
 
     def readLOG(self):
-        logFile = open("/var/log/mosquitto/mosquitto.log", mode = 'r')
+        logFile = open("/var/log/mosquitto/mosquitto.log", mode= 'r')
         logRead = logFile.read()
         logFile.close()
         print(logRead)
-        return logRead
