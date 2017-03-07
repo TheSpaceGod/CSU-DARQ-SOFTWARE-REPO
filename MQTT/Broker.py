00d3146fe8c6b17c154cfff592cc26ee8a197210
TheSpaceGod <john.rya@hotmail.com>
2017-03-06 17:25:03 -0700

    Newest Changes

--- MQTT/Broker.py
+++ MQTT/Broker.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python
+# This program is currently only built for linux.
 
 import os
 import subprocess
 import time
 
 class Broker:
-    def __init__(self):
+    def __init__(self, debug=False):
+        self.debug = debug
         euid = os.geteuid()
         if euid != 0:
             print("Need to be root to run Mosquitto Broker.")
             exit()
-        self.PnullOUT = open(os.devnull, 'w')   # Disregard process output
-        try:
-            self.Bproc = subprocess.Popen('ls', stdout=self.PnullOUT, stderr=subprocess.PIPE)   # Dummy Process to initialize Popen
-        except subprocess.CalledProcessError:
-            print("Dummy Process 'ls' failed, are you running linux? Exiting Broker.")
-            exit()
+        if self.debug: print("Broker created.")
 
     def start(self):
-        if not self.Bproc.poll() == None:
-            try:
-                self.Bproc = subprocess.Popen(['sudo', '/etc/init.d/mosquitto', 'start'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            except subprocess.CalledProcessError as e:
-                print("Failed to start Mosquitto Broker. Error: ", e)
-        else:
-            print("Broker already running, run the stop function to stop Broker.")
+        try:
+            p = subprocess.Popen(['sudo', '/etc/init.d/mosquitto', 'start'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if self.debug: print("Waiting for Broker to start.")
+            time.sleep(2)
+            if self.debug: print("Broker started.")
+        except subprocess.CalledProcessError as e:
+            print("Failed to start Mosquitto Broker. Error: ", e)
 
     def stop(self):
         try:
             p = subprocess.Popen(['sudo', '/etc/init.d/mosquitto', 'stop'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            while self.Bproc.poll() == None:
-                time.sleep(1)
+            if self.debug: print("Broker stopped.")
         except subprocess.CalledProcessError as e:
             print("Error encountered while stopping Mosquitto Broker. Error: ", e)
 
